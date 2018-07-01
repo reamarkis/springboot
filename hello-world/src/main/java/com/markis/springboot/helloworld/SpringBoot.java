2efc4a315ac2a62cc1240e6620415b72cb777b30
Rea Rebeba Markis <rea.markis@gmx.com>
2018-07-01 11:39:32 +0200

    MISC initial

---
+++ hello-world/src/main/java/com/markis/springboot/helloworld/SpringBoot.java
@@ -0,0 +1,12 @@
+package com.markis.springboot.helloworld;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class SpringBoot {
+	
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(SpringBoot.class, args);
+	}
+}
